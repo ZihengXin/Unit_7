14d88d0bc754c8358ff8445be3675294d274c7a9
Starmicks <ziheng.xin@ssfs.org>
2020-03-03 18:58:25 -0500

    3.3 Donor almost done

---
+++ src/DonorRunner.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DonorRunner {
+    public static ArrayList<Donor> readFile()throws FileNotFoundException {
+        Scanner input = new Scanner(new File("src/donors.txt"));
+        ArrayList<Donor> List = new ArrayList<>();
+        while(input.hasNextLine()){
+            String Line = input.nextLine();
+            String name = Line.substring(0, Line.indexOf(" "));
+            String number = Line.substring(Line.indexOf(" "));
+            Donor result = new Donor(name,Double. parseDouble(number) );
+            List.add(result);
+        }
+        return List;
+    }
+    public static void sortDonors(ArrayList<Donor> List) {
+        
+    }
+}
--- src/Sorting.java
+++ src/Sorting.java
@@ -1,2 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Sorting {
+    public static ArrayList<Integer> getUnsorted() throws IOException {
+        Scanner input = new Scanner(new File("src/sortedData.txt"));
+        ArrayList<Integer> List = new ArrayList<>();
+        for(int i = 0; i <= 8; i++)
+            List.add(input.nextInt());
+
+        return List;
+    }
+    public static boolean isSorted(ArrayList<Integer> List){
+        for(int i = 0; i < List.size() - 1; i++){
+            if(List.get(i) > List.get(i + 1))
+                return false;
+        }
+        return true;
+    }
 }
